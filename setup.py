22b600cf0e535a987d17d4f8d2100d1ef06f1f21
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2019-05-24 11:47:03 +0100

    Allows comments in RSS feeds file

--- setup.py
+++ setup.py
@@ -13,9 +13,11 @@ for category in open("data/categories.txt"):
   db.categories.update_one({"slug": slug}, {"$set": data}, upsert=True)
 
 for feed in open("data/feeds.txt"):
-  url, source_slug, category_slug = feed[:-1].split(", ")
+  feed = feed.strip()
+  if feed == "" or feed[0] == "#":
+    continue
+  url, source_slug, category_slug = feed.split(", ")
   source_id = db.sources.find_one({"slug":source_slug})["_id"]
   category_id = db.categories.find_one({"slug":category_slug})["_id"]
   data = {"url":url, "source_id": source_id, "category_id": category_id}
   db.feeds.update_one({"url": url}, {"$set": data}, upsert=True)
-
